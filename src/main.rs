35cb880e4558f4d2cbad62c5bba4b095171dfcc7
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-01-11 11:21:08 -0600

    use true bytecode

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,117 @@
-use std::borrow::Cow;
-
 mod tokenizer;
 
 fn main() {
     let mut chunk = Chunk::default();
-    let i = chunk.add_constant(1.2);
-    chunk.write(OpCode::Constant(i), 123);
-    chunk.write(OpCode::Return, 123);
+    let constant = chunk.add_constant(Value::from(1.2));
+    chunk.write(Opcode::Constant as u8, 123);
+    chunk.write(constant, 123);
+    chunk.write(Opcode::Return as u8, 123);
     chunk.disassemble("test chunk");
 }
 
+#[derive(Debug)]
 #[repr(u8)]
-enum OpCode {
-    Constant(usize),
+enum Opcode {
+    Constant,
     Return,
 }
 
+impl From<Opcode> for u8 {
+    fn from(value: Opcode) -> Self {
+        value as u8
+    }
+}
+
+impl TryFrom<u8> for Opcode {
+    type Error = CouldNotDecodeOpcode;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        let opcode = match value {
+            0 => Self::Constant,
+            1 => Self::Return,
+            _ => return Err(CouldNotDecodeOpcode(value)),
+        };
+        Ok(opcode)
+    }
+}
+
+#[derive(Debug)]
+struct CouldNotDecodeOpcode(u8);
+
 #[derive(Default)]
 struct Chunk {
-    code: Vec<OpCode>,
+    code: Vec<u8>,
     constants: Vec<Value>,
     lines: Vec<usize>,
 }
 
 impl Chunk {
-    pub fn write(&mut self, instr: OpCode, line: usize) {
-        self.code.push(instr);
+    fn len(&self) -> usize {
+        self.code.len()
+    }
+    pub fn write(&mut self, byte: u8, line: usize) {
+        self.code.push(byte);
         self.lines.push(line);
     }
 
-    fn add_constant(&mut self, value: Value) -> usize {
+    fn add_constant(&mut self, value: Value) -> u8 {
         self.constants.push(value);
-        self.constants.len() - 1
+        (self.constants.len() - 1) as u8
     }
 
     pub fn disassemble(&self, name: &str) {
         println!("== {name} ==");
-        let mut previous_line: Option<usize> = None;
-        for (i, instr) in self.code.iter().enumerate() {
-            let this_line = self.lines[i];
-            let line_header = if let Some(prev) = previous_line {
-                if prev == this_line {
-                    Cow::Borrowed("|")
-                } else {
-                    this_line.to_string().into()
-                }
-            } else {
-                this_line.to_string().into()
-            };
-            self.disassemble_instruction(i, instr, &line_header);
-            previous_line = Some(this_line);
+        let mut offset = 0usize;
+        while offset < self.len() {
+            offset = self.disassemble_instruction(offset);
         }
     }
 
-    fn disassemble_instruction(&self, i: usize, instr: &OpCode, line_header: &str) {
-        print!("{i:04} {line_header:>4} ");
-        match instr {
-            OpCode::Return => println!("OpCode::Return"),
-            OpCode::Constant(c) => {
-                println!("OpCode::Constant {}", self.constants[*c])
+    fn disassemble_instruction(&self, offset: usize) -> usize {
+        print!("{offset:04} ");
+
+        // Print the line information.
+        if offset > 0 && self.lines[offset] == self.lines[offset - 1] {
+            print!("   | ")
+        } else {
+            print!("{:4} ", self.lines[offset])
+        }
+
+        match Opcode::try_from(self.code[offset]) {
+            Ok(Opcode::Return) => simple_instruction("OP_RETURN", offset),
+            Ok(Opcode::Constant) => self.constant_instruction("OP_CONSTANT", offset),
+            Err(CouldNotDecodeOpcode(x)) => {
+                println!("Unknown opcode {x}");
+                offset + 1
             }
         }
     }
+
+    fn constant_instruction(&self, name: &str, offset: usize) -> usize {
+        let constant: u8 = self.code[offset + 1];
+        print!("{name:<16} {constant:4} ");
+        self.constants[constant as usize].print();
+        println!();
+        offset + 2
+    }
 }
 
-type Value = f64;
+fn simple_instruction(name: &str, offset: usize) -> usize {
+    println!("{name:<16}");
+    offset + 1
+}
+
+#[derive(Debug)]
+struct Value(f64);
+
+impl From<f64> for Value {
+    fn from(value: f64) -> Self {
+        Self(value)
+    }
+}
+
+impl Value {
+    fn print(&self) {
+        print!("'{}'", self.0);
+    }
+}
